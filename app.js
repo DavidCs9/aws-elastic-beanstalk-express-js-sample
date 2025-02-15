50ea4542147a949ab400dda1f2ecb1c7ac9aa103
DavidCs9 <davidcastrosi@hotmail.com>
2025-02-15 09:37:27 -0600

    Add new text to test CICD with manual approval

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World from MacBook!'));
+app.get('/', (req, res) => res.send('Hello World from MacBook air, this is a test for the CI/CD with manual approvaly!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
